b7d43a314c9cd78b7ab5c5dc4d81ebd4217dfe9e
Солодухин Артём Валерьевич <AVaSolodukhin@sberbank.ru>
2023-04-14 20:22:07 +0300

    Update router.py

--- router.py
+++ router.py
@@ -1,15 +1,16 @@
+from transformers import pipeline
 from fastapi import APIRouter
-
-
 from models import SourceTextLen, SourceText
-##from utils import Utils
+
 
 api_router = APIRouter()
 generator = pipeline("text-generation", "gpt2")
 
+
 @api_router.get("/")
 async def root():
     return {"message": "Формализация текста"}
+
 
 @api_router.post("/generate_len/")
 def generate_len(source: SourceTextLen):
@@ -18,12 +19,3 @@ def generate_len(source: SourceTextLen):
     - **text_len**: count of output symbols
     """
     return generator.generate_text(source.text, source.text_len)
-
-
-@api_router.post("/generate_100/")
-def generate_100(source: SourceText):
-    """Text generation using text user input and const (100)
-    value of symbols count
-    - **text**: input user text
-    """
-    return generator.generate_text(source.text, 100)
