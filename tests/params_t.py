a65bf78cd5efcca184ee8dbd8bc032be1268672f
Солодухин Артём Валерьевич <AVaSolodukhin@sberbank.ru>
2023-05-31 18:34:01 +0300

    Tests refactor

---
+++ tests/params_t.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('.')
+
+from models import SourceText as st
+
+
+st.text = 'Hi Linda, How is it going? ' \
+            'Sorry I haven not been in touch for such a long time but I have had exams so I have been studying every free minute. ' \
+            'Anyway, I love to hear all your news and I am hoping we can get together soon to catch up. ' \
+            'We just moved to a bigger flat so maybe you can come and visit one weekend? How is the new job? Looking forward to hearing from you! ' \
+            'Helga'
+
+st.precision = 0.9
--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -1,64 +1,36 @@
-import unittest
-import pytest
-
 import sys
 sys.path.append('.')
 
+import unittest
+import params_t as pt
 from unittest.mock import Mock
 from generator import Generator
 
-
-test_text = 'Hi Linda, How is it going? ' \
-            'Sorry I haven not been in touch for such a long time but I have had exams so I have been studying every free minute. ' \
-            'Anyway, I love to hear all your news and I am hoping we can get together soon to catch up. ' \
-            'We just moved to a bigger flat so maybe you can come and visit one weekend? How is the new job? Looking forward to hearing from you! ' \
-            'Helga'
+generator = Generator()
 
 if __name__ == '__main__':
     unittest.main()
 
-def test_generate_text_with_exception_precision_negative():
-    generator = Generator()
-    generator_mock = Mock(side_effect=Exception)
-    generator.generator = generator_mock
-
-    a = generator.generate_text(test_text, -1)
+def test_generate_text_with_error_precision_negative():
+    """Generate text with negative precision"""
+    a = generator.generate_text(pt.st.text, -1)
     assert "Error" in a.keys()
     assert a.get('Error').startswith('Precision is not valid')
 
-def test_generate_text_with_exception_precision_big():
-    generator = Generator()
-    generator_mock = Mock(side_effect=Exception)
-    generator.generator = generator_mock
-
-    a = generator.generate_text(test_text, -1)
+def test_generate_text_with_error_precision_big():
+    """Generate text with big precision"""
+    a = generator.generate_text(pt.st.text, 2)
     assert "Error" in a.keys()
     assert a.get('Error').startswith('Precision is not valid')
 
-def test_generate_text_with_exception_empty_text():
-    generator = Generator()
-    generator_mock = Mock(side_effect=Exception)
-    generator.generator = generator_mock
-
+def test_generate_text_with_error_empty_text():
+    """Generate text with empty text string"""
     a = generator.generate_text('', 1)
     assert "Error" in a.keys()
     assert a.get('Error').startswith('Text is not valid')
 
-def test_generate_text_with_exception_none_text():
-    generator = Generator()
-    generator_mock = Mock(side_effect=Exception)
-    generator.generator = generator_mock
-
+def test_generate_text_with_error_none_text():
+    """Generate text with None text"""
     a = generator.generate_text(None, 1)
     assert "Error" in a.keys()
     assert a.get('Error').startswith('Text is not valid')
-
-##Не нашел кейс при котором у нас выскакивает exception, если есть идеи, буду рад
-def test_generate_text_with_exception():
-    generator = Generator()
-    generator_mock = Mock(side_effect=Exception)
-    generator.generator = generator_mock
-
-    a = generator.generate_text("..................................................................................", 1)
-    assert "Exception" in a.keys()
-    assert a.get('Exception').startswith('Parameters are not valid')
