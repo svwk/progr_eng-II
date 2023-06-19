136efc852179982ade8e53a67bf1cb96dcfdfef5
SavSV <sv.ework@gmail.com>
2023-06-19 16:25:42 +0300

    code style

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -1,14 +1,15 @@
 import sys
-sys.path.append('.')
-
 import unittest
+
 import tests.params_t as pt
 from generator import Generator
 
+sys.path.append('.')
 generator = Generator()
 
 if __name__ == '__main__':
     unittest.main()
+
 
 def test_generate_text_with_error_precision_negative():
     """Generate text with negative precision"""
@@ -16,17 +17,20 @@ def test_generate_text_with_error_precision_negative():
     assert "Error" in a.keys()
     assert a.get('Error').startswith('Precision is not valid')
 
+
 def test_generate_text_with_error_precision_big():
     """Generate text with big precision"""
     a = generator.generate_text(pt.st.text, 2)
     assert "Error" in a.keys()
     assert a.get('Error').startswith('Precision is not valid')
 
+
 def test_generate_text_with_error_empty_text():
     """Generate text with empty text string"""
     a = generator.generate_text('', 1)
     assert "Error" in a.keys()
     assert a.get('Error').startswith('Text is not valid')
+
 
 def test_generate_text_with_error_none_text():
     """Generate text with None text"""
